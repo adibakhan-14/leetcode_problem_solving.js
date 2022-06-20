5cdc50e2f8d146efbbbc50f6be56eddb508ed790
adibakhan-14 <adibasumaiya.27@gmail.com>
2022-06-20 15:57:13 +0600

    changes

--- 1268suggestedProducts.js
+++ 1268suggestedProducts.js
@@ -1,4 +1,29 @@
+// var suggestedProducts = function(products, searchWord) {
+//     let newArr=[];
+
+//     for(let i=0; i< products.length; i++){
+//         if(products[i].includes(searchWord)){
+//             newArr.push(products[i])
+//         }
+//     }
+//     return newArr
+
+
+// };
+
 var suggestedProducts = function(products, searchWord) {
+    const newArr = [];
+    products.sort();
 
+    for(let i = 1; i <= searchWord.length; i++){
+        const substr = searchWord.substring(0,i);
+        newArr.push(products.filter(str => str.startsWith(substr)).slice(0, 3))
+    }
 
+    return newArr;
 };
+
+let products = ["mobile","mouse","moneypot","monitor","mousepad"];
+let searchWord = "monitor"
+
+console.log(suggestedProducts(products, searchWord));
